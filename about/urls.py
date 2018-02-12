8032364236359e02bb71e8ffaddc734603827cbc
sanghs3 <sanghs3@mcmaster.ca>
2018-02-12 15:59:00 -0500

    Record Working 2

--- about/urls.py
+++ about/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-     url(r'^$', views.index, name='About')
+     url(r'^$', views.index, name='About'),
+     url(r'^nltk', views.nltk, name='nltk')
+
 ]
--- about/views.py
+++ about/views.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.template import RequestContext
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import requires_csrf_token
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_protect
 
+import json
 # Create your views here.
 
 # Create your views here.
+@ensure_csrf_cookie
 def index(request):
-    view = "tt"
-    return render(request, "about/html/about.html", {'name': view})
+    context = RequestContext(request)
+    print(context)
+    return render(request, 'about/html/about.html', )
+
+
+
+
+@requires_csrf_token
+def nltk(request):
+    if request.is_ajax:
+       # do your stuff here
+        #post = request.POST.get()
+       ids = request.body.decode('utf-8')
+       print(ids)
+       return render(request, "about/html/about.html", {'name': "tt"})
+
+    else:
+        return HttpResponse(status=400)
